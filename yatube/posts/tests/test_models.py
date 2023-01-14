c487740a8e3b476b649895d05a5313133af285f3
SidSide <102471170+SidSide@users.noreply.github.com>
2023-01-14 13:51:08 +0300

    Update test_models.py

--- yatube/posts/tests/test_models.py
+++ yatube/posts/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-from posts.models import Group, Post
+from ..models import Group, Post
 
 User = get_user_model()
 
@@ -18,14 +18,14 @@ class PostModelTest(TestCase):
         )
         cls.post = Post.objects.create(
             author=cls.user,
-            text='Тестовый пост',
+            text='Тестовый пост более 15 символов',
         )
 
     def test_models_have_correct_object_names(self):
         """Проверяем, что у моделей корректно работает __str__."""
-        task_group = PostModelTest.group
+        task_group = self.group
         expected_object_name = task_group.title
         self.assertEqual(expected_object_name, str(task_group))
-        task_post = PostModelTest.post
-        expected_object_name = task_post.text
+        task_post = self.post
+        expected_object_name = task_post.text[:15]
         self.assertEqual(expected_object_name, str(task_post))
